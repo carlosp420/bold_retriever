874f4382c810bd6deb4c159ab7f473633167e1d9
carlosp420 <mycalesis@gmail.com>
2013-12-02 17:36:35 +0200

    try it out

--- bold_retriever.py
+++ bold_retriever.py
@@ -8,14 +8,42 @@ from Bio import SeqIO
 def request_id(seq_object, id):
     # input a sequence object
     # sends sequence to BOLD REST API for Identification Engine db=COX1_L640bp
+    # output a dictionary with the info
+    out = {}
+    out['seq'] = str(seq_object)
+    out['id'] = str(id)
     url = "http://boldsystems.org/index.php/Ids_xml"
     payload = { 'db': 'COX1_L640bp', 'sequence': str(seq_object) }
     r = requests.get(url, params=payload)
     if r.text != None:
         root = ET.fromstring(r.text)
-        for child in root:
-            if child.tag != None:
-                print id
+        for match in root.findall('match'):
+            similarity = match.find('similarity').text
+            out['similarity'] = similarity
+            tax_id = match.find('taxonomicidentification').text
+            out['tax_id'] = tax_id
+            break
+    else:
+        return None
+    return out
+
+
+def request_classification(obj):
+    #obj['tax_id'] = "Morpho helenor"
+    url = "http://www.boldsystems.org/index.php/API_Public/specimen/"
+    payload = { 'taxon': obj['tax_id'], 'specimen_download': 'tsv' }
+    r = requests.get(url, params=payload)
+    if r.text != "":
+        obj['classification'] = "true"
+        tsv = r.text.split("\n")[1].split("\t")
+        obj['class'] = tsv[11]
+        obj['order'] = tsv[13]
+        obj['family'] = tsv[15]
+        return obj
+    else:
+        obj['classification'] = "false"
+        return obj
+
 
 
 if len(sys.argv) < 2:
@@ -26,5 +54,15 @@ if len(sys.argv) < 2:
 f = sys.argv[1]
 
 for seq_record in SeqIO.parse(f, "fasta"):
-    print request_id(seq_record.seq, seq_record.id)
+    out = "seq_id,similarity,taxon,class,order,family\n"
+
+    obj = request_id(seq_record.seq, seq_record.id)
+    if obj != None:
+        obj = request_classification(obj)
+        out += obj['id'] + "," + obj['similarity'] + "," + obj['tax_id'] + ","
+        if obj['classification'] == "true":
+            out += obj['class'] + "," + obj['order'] + "," + obj['family'] + "\n"
+        else:
+            out += "None,None,None\n"
+    print out
 
