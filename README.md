1310c0c46199c3db2bbf290f78cf83793e61e5f5
carlosp420 <mycalesis@gmail.com>
2013-12-04 13:10:05 +0200

    working version for Eero

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Generate document by using Makefiles
 * ``make pdf``
 * ``make docx``
+
+# Run this way
+
+    `` python bold_retriever.py Ion_Tip_AA.fasta ``
--- bold_retriever.py
+++ bold_retriever.py
@@ -4,6 +4,9 @@ import xml.etree.ElementTree as ET
 import sys
 import requests
 from Bio import SeqIO
+import json
+import codecs
+
 
 def request_id(seq_object, id):
     # input a sequence object
@@ -17,11 +20,15 @@ def request_id(seq_object, id):
     r = requests.get(url, params=payload)
     if r.text != None:
         root = ET.fromstring(r.text)
+        #print r.text
         for match in root.findall('match'):
             similarity = match.find('similarity').text
             out['similarity'] = similarity
             tax_id = match.find('taxonomicidentification').text
             out['tax_id'] = tax_id
+
+            myid = match.find('ID').text
+            out['bold_id'] = myid
             break
     else:
         return None
@@ -30,15 +37,31 @@ def request_id(seq_object, id):
 
 def request_classification(obj):
     #obj['tax_id'] = "Morpho helenor"
-    url = "http://www.boldsystems.org/index.php/API_Public/specimen/"
-    payload = { 'taxon': obj['tax_id'], 'specimen_download': 'tsv' }
+    tax_id = obj['tax_id'].split(" ")
+    if len(tax_id) > 1:
+        tax_id = tax_id[0]
+    url = "http://www.boldsystems.org/index.php/API_Tax/TaxonSearch/"
+    print "i am sending this %s" % tax_id
+    payload = { 'taxName': tax_id }
     r = requests.get(url, params=payload)
     if r.text != "":
-        obj['classification'] = "true"
-        tsv = r.text.split("\n")[1].split("\t")
-        obj['class'] = tsv[11]
-        obj['order'] = tsv[13]
-        obj['family'] = tsv[15]
+        for k, v in json.loads(r.text).items():
+            taxID = k
+            break
+        print taxID
+        url = "http://www.boldsystems.org/index.php/API_Tax/TaxonData/"
+        payload = { 'taxId': taxID, 'dataTypes': 'basic', 'includeTree': 'true' }
+        req = requests.get(url, params=payload)
+        if req.text != "":
+            for key, val in json.loads(req.text).items():
+                if val['tax_rank'] == 'class':
+                    obj['class'] = val['taxon']
+                if val['tax_rank'] == 'order':
+                    obj['order'] = val['taxon']
+                if val['tax_rank'] == 'family':
+                    obj['family'] = val['taxon']
+                obj['classification'] = "true"
+            print obj
         return obj
     else:
         obj['classification'] = "false"
@@ -53,16 +76,22 @@ if len(sys.argv) < 2:
 
 f = sys.argv[1]
 
+out = "bold_id,seq_id,similarity,taxon,class,order,family\n"
+myfile = codecs.open("output.csv", "w", "utf-8")
+myfile.write(out)
+myfile.close()
 for seq_record in SeqIO.parse(f, "fasta"):
-    out = "seq_id,similarity,taxon,class,order,family\n"
-
+    out = ""
     obj = request_id(seq_record.seq, seq_record.id)
-    if obj != None:
+    if 'tax_id' in obj:
         obj = request_classification(obj)
+        out += obj['bold_id'] + ","
         out += obj['id'] + "," + obj['similarity'] + "," + obj['tax_id'] + ","
         if obj['classification'] == "true":
             out += obj['class'] + "," + obj['order'] + "," + obj['family'] + "\n"
         else:
             out += "None,None,None\n"
-    print out
+    myfile = codecs.open("output.csv", "a", "utf-8")
+    myfile.write(out)
+    myfile.close()
 
