af6e53bb370c62808dbf9fd91addf5250d4cd516
carlosp420 <mycalesis@gmail.com>
2014-08-28 12:27:13 +0300

    xml should start from first line

--- bold_retriever/Tests/test_bold_retriever.py
+++ bold_retriever/Tests/test_bold_retriever.py
@@ -91,16 +91,15 @@ class TestBoldRetriever(unittest.TestCase):
 
     def test_parse_bold_xml5(self):
         # malformed XML returned from BOLD
-        request = """
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!-- Edited by XMLSpy -->
-        <note>
-            <to>Tove</to>
-            <from>Jani</from>
-            <heading>Reminder</heading>
-            <body>Don't forget me this weekend!</body>
-        </note>
-        """
+        request = """<?xml version="1.0" encoding="UTF-8"?>
+                     <!-- Edited by XMLSpy -->
+                     <note>
+                        <to>Tove</to>
+                        <from>Jani</from>
+                        <heading>Reminder</heading>
+                        <body>Don't forget me this weekend!</body>
+                     </note>
+                  """
         seq_object = 'AAAGAATTTTAATTCGAGCTGAATTAAGTCAACCAGGAATATTTATTGGAAATGACCAAATTTATAACGTAATTGTTACAGCTCATGCTTTTATTATAATTttttttATAGTAATACCTATTATAATT'
         id = 'IonX17_rvr_ZA2013-0055_HochstetterForland_28_7_2013_10_21_Sanderling_juvenile_98;size=2'
         all_ids = []
