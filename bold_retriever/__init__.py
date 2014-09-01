25db124a448e6772c605b5fbd9ca27014f5148d9
carlosp420 <mycalesis@gmail.com>
2014-09-01 10:40:43 +0300

    start packaging

--- bold_retriever/__init__.py
+++ bold_retriever/__init__.py
@@ -1,1 +1,5 @@
+# -*- coding: utf-8 -*-
 
+__author__ = 'Carlos Pena'
+__email__ = 'mycalesis@gmail.com'
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,28 +1,46 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='bold_retriever',
     version='0.1.0',
-    url="https://github.com/carlosp420/bold_retriever",
-    author="Carlos Pena",
-    author_email="mycalesis@gmail.com",
-    maintainer="Carlos Pena",
-    maintainer_email="mycalesis@gmail.com",
-    contact="Carlos Pena",
-    contact_email="mycalesis@gmail.com",
-    license="GPL v3",
-    description="get barcoded info from BOLD",
-    long_description=open('README.md').read(),
-    platforms="any",
-    download_url="",
-    classifiers=[
-        "Programming Language :: Python",
-        ("Topic :: Scientific/Engineering :: Bio-Informatics"),
-        ("Intended Audience :: Science/Research"),
-        ("License :: OSI Approved :: GNU General Public License v3 (GPLv3)"),
-        ("Operating System :: OS Independent"),
-        ("Environment :: Console"),
+    description='It queries the BOLD database to get identification of taxa ' \
+                'based on COI sequences',
+    long_description=readme + '\n\n' + history,
+    author='Carlos Pena',
+    author_email='mycalesis@gmail.com',
+    maintainer='Carlos Pena',
+    url='https://github.com/carlosp420/bold_retriever',
+    packages=[
+        'bold_retriever',
     ],
-    packages=['bold_retriever'],
+    package_dir={'bold_retriever':
+                 'bold_retriever'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="GPL v3",
+    zip_safe=False,
+    keywords='bold_retriever',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+		'Programming Language :: Python',
+		'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
