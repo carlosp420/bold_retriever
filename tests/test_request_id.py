653d6bdb901c0108499db89da2a3903707728398
carlosp420 <mycalesis@gmail.com>
2014-09-03 15:54:11 +0300

    test_request_id when requests returns none

--- tests/test_request_id.py
+++ tests/test_request_id.py
@@ -34,20 +34,8 @@ class TestBoldRetriever(unittest.TestCase):
         self.assertEqual(results, expected)
 
     def test_request_id_when_request_returns_none(self):
-        results = br.request_id(self.seq, self.id, self.db)
-        results = results[0]
-        expected = {
-            'bold_id': 'SAMOS029-09',
-            'collection_country': 'Canada',
-            'seq': 'AAAGAATTTTAATTCGAGCTGAATTAAGTCAACCAGGAATATTTATTGGAAATG'
-                   'ACCAAATTTATAACGTAATTGTTACAGCTCATGCTTTTATTATAATTttttttA'
-                   'TAGTAATACCTATTATAATT',
-            'similarity': '1',
-            'tax_id': 'Diptera',
-            'id': 'IonX17_rvr_ZA2013-0055_HochstetterForland_28_7_2013_10_'
-                  '21_Sanderling_juvenile_98;size=2',
-        }
-        self.assertEqual(results, expected)
+        results = br.request_id("", self.id, self.db)
+        self.assertEqual(results, [])
 
 
 if __name__ == "__main__":
