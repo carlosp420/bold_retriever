3cb5a36a0e310914a99d2d60f1c95eb88e5d591b
carlosp420 <mycalesis@gmail.com>
2018-05-19 15:39:53 -0500

    simplified test data

--- tests/test_bold_retriever.py
+++ tests/test_bold_retriever.py
@@ -1,10 +1,8 @@
 import codecs
 import os
-import unittest
-import sys
 import json
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
@@ -31,100 +29,6 @@ class TestBoldRetriever(unittest.TestCase):
             result = br.get_bin("some taxon id")
             self.assertEqual("BOLD:AAA3750", result)
 
-    def test_parse_bold_xml1(self):
-        with open(os.path.join(TEST_FILE_PATH, "response1.xml"), "r") as handle:
-            response1 = handle.read()
-
-        seq_object = 'AAAGAATTTTAATTCGAGCTGAATTAAGTCAACCAGGAATATTTATTGGAAAT' \
-                     'GACCAAATTTATAACGTAATTGTTACAGCTCATGCTTTTATTATAATTttttt' \
-                     'tATAGTAATACCTATTATAATT'
-        id = 'IonX17_rvr_ZA2013-0055_HochstetterForland_28_7_2013_10_21_San' \
-             'derling_juvenile_98;size=2'
-        all_ids = []
-        taxon_list = []
-
-        # get only taxon_list
-        results = engine.parse_bold_xml(response1, seq_object, id, all_ids,
-                                        taxon_list)[1]
-        expected = 'Diptera'
-        self.assertTrue(expected in results)
-
-    def test_parse_bold_xml2(self):
-        # malformed XML returned from BOLD
-        request = []
-        seq_object = 'AAAGAATTTTAATTCGAGCTGAATTAAGTCAACCAGGAATATTTATTGGAAAT' \
-                     'GACCAAATTTATAACGTAATTGTTACAGCTCATGCTTTTATTATAATTttttt' \
-                     'tATAGTAATACCTATTATAATT'
-        id = 'IonX17_rvr_ZA2013-0055_HochstetterForland_28_7_2013_10_21_San' \
-             'derling_juvenile_98;size=2'
-        all_ids = []
-        taxon_list = []
-
-        # get only taxon_list
-        results = engine.parse_bold_xml(request, seq_object, id, all_ids,
-                                        taxon_list)[1]
-        expected = []
-        self.assertEqual(results, expected)
-
-    def test_parse_bold_xml3(self):
-        # malformed XML returned from BOLD
-        request = None
-        seq_object = 'AAAGAATTTTAATTCGAGCTGAATTAAGTCAACCAGGAATATTTATTGGAAAT' \
-                     'GACCAAATTTATAACGTAATTGTTACAGCTCATGCTTTTATTATAATTttttt' \
-                     'tATAGTAATACCTATTATAATT'
-        id = 'IonX17_rvr_ZA2013-0055_HochstetterForland_28_7_2013_10_21_San' \
-             'derling_juvenile_98;size=2'
-        all_ids = []
-        taxon_list = []
-
-        # get only taxon_list
-        results = engine.parse_bold_xml(request, seq_object, id, all_ids,
-                                        taxon_list)[1]
-        expected = []
-        self.assertEqual(results, expected)
-
-    def test_parse_bold_xml4(self):
-        # malformed XML returned from BOLD
-        request = "TEXT"
-        seq_object = 'AAAGAATTTTAATTCGAGCTGAATTAAGTCAACCAGGAATATTTATTGGAAAT' \
-                     'GACCAAATTTATAACGTAATTGTTACAGCTCATGCTTTTATTATAATTttttt' \
-                     'tATAGTAATACCTATTATAATT'
-        id = 'IonX17_rvr_ZA2013-0055_HochstetterForland_28_7_2013_10_21_San' \
-             'derling_juvenile_98;size=2'
-        all_ids = []
-        taxon_list = []
-
-        # get only taxon_list
-        results = engine.parse_bold_xml(request, seq_object, id, all_ids,
-                                        taxon_list)[1]
-        expected = []
-        self.assertEqual(results, expected)
-
-    def test_parse_bold_xml5(self):
-        # malformed XML returned from BOLD
-        request = """<?xml version="1.0" encoding="UTF-8"?>
-                     <!-- Edited by XMLSpy -->
-                     <note>
-                        <to>Tove</to>
-                        <from>Jani</from>
-                        <heading>Reminder</heading>
-                        <body>Don't forget me this weekend!</body>
-                     </note>
-                  """
-        seq_object = 'AAAGAATTTTAATTCGAGCTGAATTAAGTCAACCAGGAATATTTATTGGAAAT' \
-                     'GACCAAATTTATAACGTAATTGTTACAGCTCATGCTTTTATTATAATTttttt' \
-                     'tATAGTAATACCTATTATAATT'
-        id = 'IonX17_rvr_ZA2013-0055_HochstetterForland_28_7_2013_10_21_San' \
-             'derling_juvenile_98;size=2'
-        all_ids = []
-        taxon_list = []
-
-        # get only taxon_list
-        results = engine.parse_bold_xml(request, seq_object, id, all_ids,
-                                        taxon_list)[1]
-        expected = []
-        self.assertEqual(results, expected)
-
     def test_create_output_file(self):
         result = br.create_output_file("my_fasta_file.fas")
         expected = "my_fasta_file.fas_output.csv"
@@ -136,45 +40,6 @@ class TestBoldRetriever(unittest.TestCase):
             os.remove(expected)
         br.create_output_file("my_fasta_file.fas")
         self.assertTrue(os.path.isfile(expected))
-
-    def test_create_output_file_check_headers(self):
-        expected = "my_fasta_file.fas_output.csv"
-        if os.path.isfile(expected):
-            os.remove(expected)
-        br.create_output_file("my_fasta_file.fas")
-
-        with open(expected, "r") as f:
-            contents = f.readlines()
-        headers = contents[0]
-
-        self.assertTrue(os.path.isfile(expected))
-
-    def test_process_classification(self):
-        obj = {
-            'classification': 'true',
-            'class': 'Insecta',
-            'order': 'Lepidoptera',
-            'family': 'Nymphalidae',
-        }
-        expected = "Insecta,Lepidoptera,Nymphalidae"
-        result = engine.process_classification(obj)
-        self.assertEqual(expected, result)
-
-    def test_process_classification_class_none(self):
-        obj = {
-            'classification': 'true',
-        }
-        expected = "None,None,None"
-        result = engine.process_classification(obj)
-        self.assertEqual(expected, result)
-
-    def test_process_classification_false(self):
-        obj = {
-            'classification': 'false',
-        }
-        expected = "None,None,None"
-        result = engine.process_classification(obj)
-        self.assertEqual(expected, result)
 
     def test_generate_output_content(self):
         output_filename = 'ionx13.fas_output.csv'
@@ -194,20 +59,11 @@ class TestBoldRetriever(unittest.TestCase):
         self.assertIn("SIOCA145-10", result.strip())
         self.assertIn("Canada", result.strip())
 
-    def test_get_tax_id_from_web(self):
-        obj = {'division': 'animal',
-               'classification': 'true',
-               'seq': 'AATTTGATCAGGTTTAGTAGGAACTAGATTAAGTTTATTAATTCGAGCCGAATTAGGTCAACCAGGTTCATTAATTGGAGATGACCAAATTTATAATGTAATCGTAACTGCTCATGCATTTATTATAATTTTCTTCATAGTTATACCTATTGTTATTGGA',
-               'similarity': '1',
-               'class': u'Insecta',
-               'collection_country': 'Finland',
-               'taxID': u'107',
-               'bold_id': 'NEUFI079-11',
-               'order': u'Neuroptera',
-               'id': 'OTU_99',
-               'tax_id': 'Neuroptera'}
-        results = engine.get_tax_id_from_web(obj)
-        self.assertEqual('Neuroptera', results['tax_id'])
+    def tearDown(self):
+        files = ["ionx13.fas_output.csv", "my_fasta_file.fas_output.csv"]
+        for filename in files:
+            if os.path.isfile(filename):
+                os.remove(filename)
 
 
 if __name__ == "__main__":
